92a8f954ab260cc8df4953fdff020ab6559f28a8
habeeb <habeebomotolani2007@gmail.com>
2024-03-06 05:56:01 -0800

    edited dropdown

--- home/forms.py
+++ home/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class ConversionForm(forms.Form):
-    convert_to = forms.ChoiceField(choices=[('Morse', 'Morse'), ('English', 'English'),], widget=forms.Select(attrs={'class': 'box dropdown-box'}), label='')
+    convert_to = forms.ChoiceField(choices=[('Morse', 'Translate to Morse'), ('English', 'Translate to English'),], widget=forms.Select(attrs={'class': 'box dropdown-box'}), label='')
     text = forms.CharField(widget=forms.Textarea(attrs={'class': 'text', 'placeholder':'Enter Text'}), label='')
