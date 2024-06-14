a64dedc0e5b7474bf891dfd455cbd2a105e0efe5
habeeb <143030996+habeebsl@users.noreply.github.com>
2024-06-14 14:43:55 +0100

    Update settings.py

--- Morse_code_converter/settings.py
+++ Morse_code_converter/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'Morse_code_converter.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
