e723b2fdb491438821e00b20b391b00772d5d3f0
habeeb <143030996+habeebsl@users.noreply.github.com>
2024-06-14 04:59:20 -0800

    Update settings.py

--- Morse_code_converter/settings.py
+++ Morse_code_converter/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'Morse_code_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 }
 
 # Password validation
