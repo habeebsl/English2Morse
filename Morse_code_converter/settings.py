ee5a0a4da9a3d0bf5f130736fe6da5f13fb8f7af
habeeb <habeebomotolani2007@gmail.com>
2024-06-14 04:23:09 -0800

    added api endpoints

--- Morse_code_converter/settings.py
+++ Morse_code_converter/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Or any other location you prefer
 
