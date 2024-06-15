955927f3fa53c3cb731d533d64d59e3609cfa8f4
habeeb <habeebomotolani2007@gmail.com>
2024-06-14 16:01:24 -0800

    added api endpoints

--- Morse_code_converter/settings.py
+++ Morse_code_converter/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'Morse_code_converter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 
-# import dj_database_url
+import dj_database_url
 
-# DATABASES = {
-#     'default':dj_database_url.parse(os.getenv("DATABASE_URL"))
-# }
+DATABASES = {
+    'default':dj_database_url.parse(os.getenv("DATABASE_URL"))
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
